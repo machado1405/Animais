df3fa2bd3e6a7046dcdf30bedbb19a24ceb28428
Matheus Machado <agromsmachado@gmail.com>
2021-03-01 20:14:43 -0300

    Selecionar tags e itens no html

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exemplos de como selecionar tags e elementos no html
+// const animais = document.getElementById('animais');
+// const gridSection = document.getElementsByClassName('grid-section');
+// const primeiraLi = document.querySelector('li');
+// const primeiraUl = document.querySelector('ul');
+// const linkInterno = document.querySelector('[href^="#"]');
+// const animaisImg = document.querySelectorAll('.animais img');
