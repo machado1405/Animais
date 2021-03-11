87115d5d627dd05005a867af9a92ca519bd850a7
Matheus Machado <agromsmachado@gmail.com>
2021-03-11 16:09:01 -0300

    Estudo js seleçao elementos

--- js-selecao-elementos.js
+++ js-selecao-elementos.js
@@ -21,3 +21,93 @@ console.log(animaisH2);
 
 const paragrafos = document.querySelectorAll('p');
 console.log(paragrafos[paragrafos.length - 1]);
+
+// forEach selecionar uma lista de itens do dom
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach(function(item, index, array){
+//     console.log(item, index, array);
+// });
+
+// forEach é um método array, alguns arrays-like possuem esse método
+// caso não possua o ideial é transformá-los em um array
+
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos);
+
+// titulosArray.forEach(function(item){
+//     console.log(item);
+// });
+
+
+// Alternativa de escrita, removendo o function e usando => (fat arrow)
+// 1 unico argumento n precisa de () forEach() => {}
+// const imgs = document.querySelectorAll('img');
+// imgs.forEach((item) => {
+//     console.log(item);
+// }) 
+
+// const paragrafo = document.querySelectorAll('p');
+// paragrafo.forEach(function(item){
+//     console.log(item.innerText);
+// });
+
+// const imgs = document.querySelectorAll('img');
+// imgs.forEach(function(item, index){
+//     console.log(item, index);
+// })
+
+// let i = 0;
+// imgs.forEach(() => {
+//     console.log(i++);
+// });
+
+// imgs.forEach(() => console.log(i++));
+
+const menu = document.querySelector('.menu');
+
+menu.classList.add('ativo', 'teste');
+menu.classList.remove('teste');
+menu.classList.toggle('azul'); // adiciona ou remove
+
+if (menu.classList.contains('azul')){
+    menu.classList.add('possui-azul');
+}else {
+    menu.classList.add('nao-possui-azul');
+}
+
+// console.log(menu.classList);
+
+const img = document.querySelector('img');
+img.setAttribute('alt', 'é uma raposa');
+const possuiAlt = img.hasAttribute('alt');
+img.removeAttribute('alt');
+
+// console.log(possuiAlt);
+// console.log(img.getAttribute('src'));
+
+// adiciona ao 'a' a classe ativo
+const itensMenu = document.querySelectorAll('.menu a');
+itensMenu.forEach((item) => {
+   item.classList.add('ativo'); 
+});
+
+itensMenu.forEach((item) => {
+    item.classList.remove('ativo'); 
+ });
+itensMenu[0].classList.add('ativo');
+
+console.log(itensMenu);
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(img, possuiAtributo);
+});
+
+const link = document.querySelector('a[href^="http"]');
+link.setAttribute('href', 'https://google.com');
+
+console.log(link);
--- script.js
+++ script.js
@@ -1,44 +1,6 @@
-// forEach selecionar uma lista de itens do dom
+const section = document.querySelector('.animais');
 
-// const imgs = document.querySelectorAll('img');
-
-// imgs.forEach(function(item, index, array){
-//     console.log(item, index, array);
-// });
-
-// forEach é um método array, alguns arrays-like possuem esse método
-// caso não possua o ideial é transformá-los em um array
-
-// const titulos = document.getElementsByClassName('titulo');
-// const titulosArray = Array.from(titulos);
-
-// titulosArray.forEach(function(item){
-//     console.log(item);
-// });
-
-
-// Alternativa de escrita, removendo o function e usando => (fat arrow)
-// 1 unico argumento n precisa de () forEach() => {}
-// const imgs = document.querySelectorAll('img');
-// imgs.forEach((item) => {
-//     console.log(item);
-// }) 
-
-// const paragrafo = document.querySelectorAll('p');
-// paragrafo.forEach(function(item){
-//     console.log(item.innerText);
-// });
-
-// const imgs = document.querySelectorAll('img');
-// imgs.forEach(function(item, index){
-//     console.log(item, index);
-// })
-
-// let i = 0;
-// imgs.forEach(() => {
-//     console.log(i++);
-// });
-
-// imgs.forEach(() => console.log(i++));
-
+section.clientHeight; // height + padding
+section.offsetHeight; // height + padding + border
+section.scrollHeight; // height total, mesmo dentro de scroll
 
