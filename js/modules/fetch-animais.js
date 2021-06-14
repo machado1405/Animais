8e246a998754076b4aa9c9fa3fb5fb2f0e76e75f
Matheus Machado <agromsmachado@gmail.com>
2021-06-14 20:32:19 -0300

    Refatorar anima numeros e fetch animais

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,20 +1,7 @@
+import AnimaNumeros from './anima-numeros.js';
 import initAnimaNumeros from './anima-numeros.js';
-export default function initFetchAnimais() {
-    async function fetchAnimais(url) {
-        try {
-            const animaisResponse = await fetch(url);
-            const animaisJSON = await animaisResponse.json();
-            const numerosGrid = document.querySelector('.numeros-grid');
-            animaisJSON.forEach(animais => {
-                const divAnimal = createAnimal(animais);
-                numerosGrid.appendChild(divAnimal);
-            });
-            initAnimaNumeros();
-        } catch(erro) {
-            console.log(erro);
-        }
-    };
-
+export default function fetachAnimais(url, target) {    
+    // Cria a div contendo informações com o total de animais
     function createAnimal(animal) {
         const div = document.createElement('div');
         div.classList.add('numero-animal');
@@ -22,5 +9,32 @@ export default function initFetchAnimais() {
         return div;
     }
 
-    fetchAnimais('./animaisAPI.json');
+    // Preenche cada animal no DOM
+    const numerosGrid = document.querySelector(target);
+    function preencherAnimais(animal) {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+    };
+
+    // Anima os números de cada animal
+    function animaAnimaisNumeros() {
+        const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+        animaNumeros.init();
+    }
+
+    // Puxa os animais através de um json
+    // e cria cada animal utilizando createAnimal
+    async function criarAnimais() {
+        try {
+            const animaisResponse = await fetch(url);
+            // Transforma a resposta em JSON
+            const animaisJSON = await animaisResponse.json();
+            animaisJSON.forEach(animal => preencherAnimais(animal));
+            animaAnimaisNumeros();
+        } catch(erro) {
+            console.log(erro);
+        }
+    };
+
+    return criarAnimais();
 }
--- js/script-efeitos.js
+++ js/script-efeitos.js
@@ -3,11 +3,11 @@
 // import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
 // import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+// import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento-horario.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 // initTabNav();
@@ -15,11 +15,11 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 // initScrollSuave();
 initAnimacaoScroll();
 // initModal();
-initTooltip();
+// initTooltip();
 initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
 initFetchBitcoin();
 
 // IMPORTAÇÕES REFATORAÇÃO
@@ -28,6 +28,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js'
 import Modal from './modules/modal.js'
+import Tooltip from './modules/tooltip.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -40,3 +41,6 @@ tabNav.init();
 
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
+
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
